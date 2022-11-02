ef01c93f88839f5c5b6079abbe51b5ef48895015
Notarin <424c414e4b@gmail.com>
2022-11-02 01:45:26 -0400

    Moved commands to their own class

---
+++ src/main/java/com/notarin/pride_Bot/Commands.java
@@ -0,0 +1,21 @@
+package com.notarin.pride_Bot;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.util.Objects;
+
+public class Commands {
+    public static void say(SlashCommandInteractionEvent event, String content) {
+        event.reply(content).queue(); // This requires no permissions!
+    }
+
+    public static void leave(SlashCommandInteractionEvent event) {
+        if (!Objects.requireNonNull(event.getMember()).hasPermission(Permission.KICK_MEMBERS))
+            event.reply("You do not have permissions to kick me.").setEphemeral(true).queue();
+        else event.reply("Leaving the server... :wave:") // Yep we received it
+                .flatMap(v -> Objects.requireNonNull(event.getGuild()).leave()) // Leave server after acknowledging the command
+                .queue();
+    }
+
+}
--- src/main/java/com/notarin/pride_Bot/Main.java
+++ src/main/java/com/notarin/pride_Bot/Main.java
@@ -2,11 +2,9 @@ package com.notarin.pride_Bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.yaml.snakeyaml.Yaml;
@@ -34,12 +32,12 @@ public class Main extends ListenerAdapter {
 
         // Simple reply commands
         //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(Commands.slash("say", "Makes the bot say what you tell it to").addOption(STRING, "content", "What the bot should say", true) // you can add required options like this too
+        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("say", "Makes the bot say what you tell it to").addOption(STRING, "content", "What the bot should say", true) // you can add required options like this too
         );
 
         // Commands without any inputs
         //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(Commands.slash("leave", "Make the bot leave the server").setGuildOnly(true) // this doesn't make sense in DMs
+        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("leave", "Make the bot leave the server").setGuildOnly(true) // this doesn't make sense in DMs
                 .setDefaultPermissions(DefaultMemberPermissions.DISABLED) // only admins should be able to use this command.
         );
 
@@ -54,21 +52,9 @@ public class Main extends ListenerAdapter {
         if (event.getGuild() == null) return;
         switch (event.getName()) {
             case "say" ->
-                    say(event, Objects.requireNonNull(event.getOption("content")).getAsString()); // content is required so no null-check here
-            case "leave" -> leave(event);
+                    Commands.say(event, Objects.requireNonNull(event.getOption("content")).getAsString()); // content is required so no null-check here
+            case "leave" -> Commands.leave(event);
             default -> event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
         }
-    }
-
-    public void say(SlashCommandInteractionEvent event, String content) {
-        event.reply(content).queue(); // This requires no permissions!
-    }
-
-    public void leave(SlashCommandInteractionEvent event) {
-        if (!Objects.requireNonNull(event.getMember()).hasPermission(Permission.KICK_MEMBERS))
-            event.reply("You do not have permissions to kick me.").setEphemeral(true).queue();
-        else event.reply("Leaving the server... :wave:") // Yep we received it
-                .flatMap(v -> Objects.requireNonNull(event.getGuild()).leave()) // Leave server after acknowledging the command
-                .queue();
     }
 }
