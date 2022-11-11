2c4bbc41b339049b29ca31b375e661b825e6adf2
Notarin <424c414e4b@gmail.com>
2022-11-11 02:01:16 -0500

    Moved slash command registration to its own class

--- src/main/java/com/notarin/pride_Bot/Main.java
+++ src/main/java/com/notarin/pride_Bot/Main.java
@@ -4,9 +4,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileInputStream;
@@ -14,8 +12,6 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.EnumSet;
 import java.util.Map;
-
-import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) {
@@ -41,32 +37,8 @@ public class Main extends ListenerAdapter {
             throw new RuntimeException(e);
         }
 
-        // Prepare building command list
-        CommandListUpdateAction commands = jda.updateCommands();
-
-        //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("ping", "Test connectivity").setGuildOnly(true) // Allow only in Guilds/Servers
-        );
-
-        //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("say", "Makes the bot say what you tell it to").addOption(STRING, "content", "What the bot should say", true) // you can add required options like this too
-        );
-
-        //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("leave", "Make the bot leave the server").setGuildOnly(true) // Allow only in Guilds/Servers
-                .setDefaultPermissions(DefaultMemberPermissions.DISABLED)
-        );
-
-        //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("suggest", "For when you want to make a suggestion").setGuildOnly(true) // Allow only in Guilds/Servers
-        );
-
-        //noinspection ResultOfMethodCallIgnored
-        commands.addCommands(net.dv8tion.jda.api.interactions.commands.build.Commands.slash("bug", "For when you want to report a bug").setGuildOnly(true) // Allow only in Guilds/Servers
-        );
-
-        // Send the new set of commands to discord, this will override any existing global commands with the new set provided here
-        commands.queue();
+        //register the slash commands
+        SlashCommandRegistrar.register(jda);
     }
 
 
