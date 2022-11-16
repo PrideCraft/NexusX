1d9360e194a382282d481f42fecea14068a24100
Notarin <424c414e4b@gmail.com>
2022-11-16 04:46:27 -0500

    added updateCheck() method

--- src/main/java/com/notarin/pride_Bot/ConfigHandler.java
+++ src/main/java/com/notarin/pride_Bot/ConfigHandler.java
@@ -22,4 +22,39 @@ public class ConfigHandler {
         }
         return config;
     }
+
+    public static void updateCheck(Map<String, Object> config) {
+        Map<String, Object> exampleConfig;
+
+        try {
+            //Loading config file
+            InputStream inputStream = new FileInputStream("config-example.yml");
+            Yaml yaml = new Yaml();
+            //set the config object
+            exampleConfig = yaml.load(inputStream);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        //check for unconfigured values in config, or useless values in config
+        for (Map.Entry<String, Object> configIteration : config.entrySet()) {
+            //Check for keys in config not present in exampleconfig, this isn't really a problem so just warn.
+            if (exampleConfig.get(configIteration.getKey()) == null) {
+                System.out.println("[WARNING] Key found in config not present in example config: " + configIteration.getKey());
+            }
+            if (configIteration.getValue().equals(exampleConfig.get(configIteration.getKey()))) {
+                System.out.println("[ERROR] Config value unchanged, please configure key \"" + configIteration.getKey() + "\"");
+                System.exit(1);
+            }
+        }
+
+        //check for missing keys in config
+        for (Map.Entry<String, Object> exampleConfigIteration : exampleConfig.entrySet()) {
+            if (config.get(exampleConfigIteration.getKey()) == null) {
+                System.out.println("[ERROR] Key found in example config not present in config: " + exampleConfigIteration.getKey() +
+                "\nPlease copy all missing values and set them.");
+                System.exit(1);
+            }
+        }
+    }
 }
