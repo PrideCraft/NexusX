e6b63f176b823a35ec1c385a41d6b30da26376bc
Notarin <424c414e4b@gmail.com>
2023-01-30 03:44:11 -0500

    Created Logging
    Set up logging using the LogHandler class, simply pass a name/category for the log name, and the text to log.

--- src/main/java/com/notarin/pride_craft_network/Main.java
+++ src/main/java/com/notarin/pride_craft_network/Main.java
@@ -3,6 +3,8 @@ package com.notarin.pride_craft_network;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
+import static com.notarin.pride_craft_network.LogHandler.logInfo;
+
 public class Main {
 
     public static void main(String[] args) throws InvocationTargetException,
@@ -10,6 +12,7 @@ public class Main {
         Map<String, Object> config = ConfigHandler.loadConfig();
 
         com.notarin.pride_craft_network.discord_bot.Main.init(config);
+        logInfo("Main", "Pride Craft Network started!");
     }
 
 }
