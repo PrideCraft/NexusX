751cb8d809282bacb5e5de7354c5a14250e5bab6
Notarin <424c414e4b@gmail.com>
2023-01-30 03:44:11 -0500

    Created Logging
    Set up logging using the LogHandler class, simply pass a name/category for the log name, and the text to log.

---
+++ src/main/java/com/notarin/pride_craft_network/LogHandler.java
@@ -0,0 +1,30 @@
+package com.notarin.pride_craft_network;
+
+@SuppressWarnings("unused")
+public class LogHandler {
+
+    static void logInfo(String loggerName, String message) {
+        if (org.slf4j.LoggerFactory.getLogger(loggerName).isInfoEnabled()) {
+            org.slf4j.LoggerFactory.getLogger(loggerName).info(message);
+        }
+    }
+
+    static void logWarn(String loggerName, String message) {
+        if (org.slf4j.LoggerFactory.getLogger(loggerName).isWarnEnabled()) {
+            org.slf4j.LoggerFactory.getLogger(loggerName).warn(message);
+        }
+    }
+
+    static void logError(String loggerName, String message) {
+        if (org.slf4j.LoggerFactory.getLogger(loggerName).isErrorEnabled()) {
+            org.slf4j.LoggerFactory.getLogger(loggerName).error(message);
+        }
+    }
+
+    static void logDebug(String loggerName, String message) {
+        if (org.slf4j.LoggerFactory.getLogger(loggerName).isDebugEnabled()) {
+            org.slf4j.LoggerFactory.getLogger(loggerName).debug(message);
+        }
+    }
+
+}
