29c53a2000fb91656caa16fa4cf263746cb92812
Notarin <424c414e4b@gmail.com>
2023-01-31 06:22:39 -0500

    Added a simple webserver
    Added a web server utilizing REST and HTTP, added a pom.xml dependency for Spark (the driver for http), and added configuration options for added features (port and secrets/tokens).

--- src/main/java/com/notarin/pride_craft_network/Main.java
+++ src/main/java/com/notarin/pride_craft_network/Main.java
@@ -1,14 +1,15 @@
 package com.notarin.pride_craft_network;
 
 import java.lang.reflect.InvocationTargetException;
-
 import static com.notarin.pride_craft_network.LogHandler.logInfo;
 
 public class Main {
 
     public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
+            NoSuchMethodException, InstantiationException,
+            IllegalAccessException {
         com.notarin.pride_craft_network.discord_bot.Main.init();
+        com.notarin.pride_craft_network.web_server.Main.init();
         logInfo("Main", "Pride Craft Network started!");
     }
 
