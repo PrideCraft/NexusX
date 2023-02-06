7e828b2ab8ded862bccf5451a7ea0f3ac363870a
Notarin <424c414e4b@gmail.com>
2023-02-06 03:41:12 -0500

    Moved web server Routes to their own Class

---
+++ src/main/java/com/notarin/pride_craft_network/web_server/Routes.java
@@ -0,0 +1,37 @@
+package com.notarin.pride_craft_network.web_server;
+
+import com.notarin.pride_craft_network.database.objects.PrideUser;
+import spark.Spark;
+
+import static com.notarin.pride_craft_network.database.Query.createAccount;
+
+/**
+ * The routes class for the web server.
+ * This class contains all the routes for the web server.
+ */
+public class Routes {
+    static void makeUserFromMinecraftUuid() {
+        Spark.post("/make-user/minecraft-uuid/:uuid", (req, res) -> {
+            if (Main.elevatedTransaction(req)) {
+                final String regex = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$";
+                if (!req.params(":uuid").matches(regex)) {
+                    res.status(400);
+                    return BuildJson.error("Invalid UUID");
+                }
+                final PrideUser account = createAccount(req.params(":uuid"));
+                return Main.getUser(res, account.id());
+            } else return Main.denyTransaction(res);
+        });
+    }
+
+    static void getUser() {
+        Spark.get("/user/:id", (req, res) -> {
+            final String params = req.params(":id");
+            return Main.getUser(res, params);
+        });
+    }
+
+    static void ping() {
+        Spark.get("/ping", (req, res) -> "Pong!");
+    }
+}
