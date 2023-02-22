a8abde11c0c8a29491b17ccc333d1b7a74955b9c
Notarin <424c414e4b@gmail.com>
2023-02-22 05:59:52 -0500

    Added register command

---
+++ src/main/java/com/notarin/pride_craft_network/discord_bot/slash_commands/commands/Register.java
@@ -0,0 +1,39 @@
+package com.notarin.pride_craft_network.discord_bot.slash_commands.commands;
+
+import com.notarin.pride_craft_network.database.Query;
+import com.notarin.pride_craft_network.database.objects.PrideUser;
+import com.notarin.pride_craft_network.discord_bot.slash_commands.SlashCommandHandler;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
+/**
+ * A command for testing the bot connectivity.
+ */
+@SuppressWarnings("unused")
+public class Register implements SlashCommandHandler {
+
+    @Override
+    public String getName() {
+        return "register";
+    }
+
+    @Override
+    public String getDescription() {
+        return "register to the pride craft network";
+    }
+
+    @Override
+    public void handle(final SlashCommandInteractionEvent event) {
+        try {
+            final String discordId = event.getUser().getId();
+            final PrideUser account = Query.createAccount();
+            Query.linkDiscordIdQuery(account, discordId);
+            event.reply("You have been registered!").queue();
+        } catch (@SuppressWarnings("OverlyBroadCatchBlock") final Exception e) {
+            event.reply("An error occurred!").queue();
+        }
+    }
+
+    @Override
+    public void setOptions(final SlashCommandData command) {}
+}
